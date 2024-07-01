41d4e2e8824c3935ccd8ace81d4f37543d948fae
NarukiAndo <5050naruboworld00@gmail.com>
2024-07-01 16:52:37 +0900

    donedone

--- src/app/api/auth/[...nextauth]/route.tsx
+++ src/app/api/auth/[...nextauth]/route.tsx
@@ -1,22 +1,6 @@
 import { nextAuthOptions } from "@/app/lib/next-auth/options";
-import NextAuth from "next-auth/next";
-import GitHubProvider from "next-auth/providers/github";
-
+import NextAuth from "next-auth";
 const handler = NextAuth(nextAuthOptions);
 
+// https://next-auth.js.org/configuration/initialization#route-handlers-app
 export { handler as GET, handler as POST };
-
-export default NextAuth({
-  providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-  callbacks: {
-    async redirect({ url, baseUrl }) {
-      return url.startsWith(baseUrl) ? url : baseUrl;
-    },
-  },
-});
